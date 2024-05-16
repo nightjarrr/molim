fe06fd7c311ce7accbd8c7d1452bdb5daa1e58b0
nightjarrr <a@b.com>
2024-05-16 15:05:18 +0200

    Improved stats and display of processing.

--- w_all.py
+++ w_all.py
@@ -8,13 +8,34 @@ import string
 import pathlib
 import argparse
 import subprocess
+from datetime import timedelta
 
 VIDEOS_EXT = '.mp4'
 CODEC = 'libx265'
 RATE = 26
 ORIGINALS_FOLDER = '_orig/'
 PROCESSED_SUFFIX = 'min'
-GREATER_THAN = '4M'
+GREATER_THAN = '30M'
+ORIGINAL_HANDLING_OPTIONS = ('leave', 'move', 'delete')
+
+class _print:
+    level = 0
+
+    def __filtered(s, lvl):
+        if lvl >= _print.level:
+            print(s)
+
+    def totals(s):
+        _print.__filtered(s, 4)
+
+    def folder(s):
+        _print.__filtered(s, 3)
+
+    def file(s):
+        _print.__filtered('\t' + s, 2)
+
+    def filedetails(s):
+        _print.__filtered('\t\t' + s, 1)
 
 def _human_to_byte(size):
     size_name = {'K':1, 'M':2, 'G':3}
@@ -37,41 +58,48 @@ class HumanReadableSizeType(object):
     def __call__(self, string):
         return _human_to_byte(string)
 
-def _prepare_processing(args, folder):
-    print(f'\nProcessing folder: {folder}')
+def _prepare_folder(args, folder, index, count):
+    _print.folder(f'\n[{index+1}/{count}] Processing folder: {folder}')
     
     ends_with = lambda f, ext: f.lower().endswith(ext.lower())
 
     file_paths = [os.path.join(folder, f) for f in os.listdir(folder)]
     files = [f for f in file_paths if os.path.isfile(f) \
         and ends_with(f, args.video_files)]
-    print(f'\tFound {len(files)} video files ({args.video_files})')
+    _print.folder(f'\tFound {len(files)} video files ({args.video_files})')
 
     result = []
+    skipped = 0
     for f in files:
         processed_mask = f'{args.suffix}{args.video_files}'
         if not args.overwrite_processed and ends_with(f, processed_mask):
-            print(f'\tSkipping {f} since it is already processed.')
+            _print.filedetails(f'- Skipping {f} since it is already processed.')
+            skipped +=1
         else:
             size = os.path.getsize(f)
             if size < args.greater_than:
-                print(f'\tSkipping {f}({_byte_to_human(size)}) since it is smaller than {_byte_to_human(args.greater_than)}.')
+                _print.filedetails(f'- Skipping {f}({_byte_to_human(size)}) since it is smaller than {_byte_to_human(args.greater_than)}.')
+                skipped +=1
             else:
                 result.append(f)
+    if skipped > 0:
+        _print.folder(f'\tSkipped {skipped} files.')
+
     if not result:
-        print(f'\n\tLooks like there is nothing to do in folder {folder}\n')
+        _print.folder(f'\n\tLooks like there is nothing to do in folder {folder}.\n')
         return None
 
-    print(f'\n\tProceeding to process {len(result)} files:')
-    print('\t' + '\n\t'.join(result))
+    _print.folder(f'\tProceeding to process {len(result)} files')
+    for f in result:
+        _print.filedetails('- ' + f)
 
     if args.originals == 'move':
         orig_folder = os.path.join(folder, args.originals_folder)
         try:
             os.mkdir(orig_folder)
-            print(f'\n\tCreated subfolder for original files: {orig_folder}')
+            _print.folder(f'\n\tCreated subfolder for original files: {orig_folder}')
         except FileExistsError:
-            print(f'\n\tSubfolder for original files already exists: {orig_folder}')
+            _print.folder(f'\n\tSubfolder for original files already exists: {orig_folder}')
 
     return result
 
@@ -82,7 +110,8 @@ def _process_file(args, folder, file, index, count):
     size = os.path.getsize(file)
     stat.original_size = size
 
-    print (f'\n\t[{index+1}/{count}] Processing {file} ({_byte_to_human(size)})')
+    _print.file('')
+    _print.file(f'[{index+1}/{count}] Processing {file} ({_byte_to_human(size)})')
     start_time = time.time()
 
     command = ['ffmpeg', '-y',  # Base command
@@ -95,42 +124,64 @@ def _process_file(args, folder, file, index, count):
     stat.output_file = output_file
     command.append(f'{output_file}')
     # Additional
-    if not args.skip_ffmpeg_report:
+    if args.ffmpeg_report:
         command.append('-report')
 
-    print (f'\t\tRunning ffmpeg to create {output_file}')
+    _print.filedetails(f'Running ffmpeg to create {output_file}')
     # ffmpeg -i "$fullpath" -vcodec libx265 -crf 26 "$newpath"
     subprocess.run(command, stdout = subprocess.DEVNULL, stderr = subprocess.STDOUT)    
+    stat.output_file_size = os.path.getsize(output_file)
 
     # original file post processing
     if args.originals == 'move':
         destination = os.path.join(folder, args.originals_folder, os.path.basename(file))
-        print(f'\t\tMoving original file to {destination}.')
+        _print.filedetails(f'Moving original file to {destination}.')
         os.rename(file, destination)
         stat.original_moved = destination
     elif args.originals == 'delete':
-        print(f'\t\tDeleting original file.')
+        _print.filedetails(f'Deleting original file.')
         os.remove(file)
 
     elapsed = time.time() - start_time
     stat.elapsed = elapsed
-    print (f'\t\tDONE ({elapsed:.2f}s)')
+    stat.delta_size = size - stat.output_file_size
+    stat.delta_percent = 100 * float(stat.output_file_size) / size
+    _print.filedetails(f'New size: {_byte_to_human(stat.output_file_size)}, ({stat.delta_percent:.1f}% of original, saved {_byte_to_human(stat.delta_size)})')
+    _print.file(f'\tDONE in {timedelta(seconds=int(elapsed))}')
 
     return stat
 
-def _run_processing(args, folder, files):
+def _process_folder(args, folder, files):
     stats = []
     i = 0
+    start_time = time.time()
     for f in files:
         s = _process_file(args, folder, f, i, len(files))
         stats.append(s)
         i += 1
-    return stats
+    elapsed = time.time() - start_time
+    return elapsed, stats
 
-def _display_stats(stats):
-    print()
+def _display_folder_stats(folder, elapsed, files_stats):
+    _print.folder(f'\n\tTotals for {folder}')
+    old_total_size = sum([stat.original_size for stat in files_stats])
+    _print.folder(f'\tProcessed {len(files_stats)} files ({_byte_to_human(old_total_size)} in total) in {timedelta(seconds=int(elapsed))}')
+    new_total_size = sum([stat.output_file_size for stat in files_stats])
+    delta_size = old_total_size - new_total_size
+    delta_percent = 100 * float(new_total_size) / old_total_size
+    _print.folder(f'\tNew total size: {_byte_to_human(new_total_size)}, ({delta_percent:.1f}% of original, saved {_byte_to_human(delta_size)})\n')
+
+def _display_total_stats(elapsed, folders_count, files_stats):
+    if files_stats:
+        old_total_size = sum([stat.original_size for stat in files_stats])
+        _print.totals(f'Processed {len(files_stats)} files in {folders_count} folder(s) ({_byte_to_human(old_total_size)}  in total) in {timedelta(seconds=int(elapsed))}')
+        new_total_size = sum([stat.output_file_size for stat in files_stats])
+        delta_size = old_total_size - new_total_size
+        delta_percent = 100 * float(new_total_size) / old_total_size
+        _print.totals(f'New total size: {_byte_to_human(new_total_size)}, ({delta_percent:.1f}% of original, saved {_byte_to_human(delta_size)})\n')
 
 def main():
+
     parser = argparse.ArgumentParser(description='Process video files with ffmpeg',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('FOLDERS', nargs='+', help = 'Process videos in these folders.')
@@ -146,23 +197,32 @@ def main():
         help='Extension of processed video files.')
     parser.add_argument('-c', '--codec', default=CODEC, help='Codec to use for processing.')
     parser.add_argument('-r', '--rate', default=RATE, type=int, help='Processing compression rate.')
-    parser.add_argument('-o', '--originals', choices=['leave', 'move', 'delete'],
+    parser.add_argument('-o', '--originals', choices=ORIGINAL_HANDLING_OPTIONS,
         default = 'move', help='How to handle original files after processing.')
     parser.add_argument('-f', '--originals-folder', default=ORIGINALS_FOLDER, help='Subfolder to save originals into.')
-    
-    parser.add_argument('--skip-ffmpeg-report', default = False, action = 'store_true',
-        help='Do not write ffmpeg report with extended conversion information')
+    parser.add_argument('--display-verbosity', type=int, choices=range(1, 4),
+        default = 2, help='Verbosity of processing progress display.')
+    parser.add_argument('--ffmpeg-report', default = False, action = 'store_true',
+        help='Write ffmpeg report with extended conversion information')
 
     args = parser.parse_args()
-    print(f'Starting processing with these parameters:\n{args}')
+    _print.level = args.display_verbosity
+    _print.totals(f'Starting processing with these parameters:\n{args}')
 
+    i = 0
+    start_time = time.time()
+    total_stats = []
     for folder in args.FOLDERS:
-        files = _prepare_processing(args, folder)
+        files = _prepare_folder(args, folder, i, len(args.FOLDERS))
         if files:
-            stats = _run_processing(args, folder, files)
-            _display_stats(stats)
+            elapsed, stats = _process_folder(args, folder, files)
+            _display_folder_stats(folder, elapsed, stats)
+            total_stats += stats
+        i += 1
+    elapsed = time.time() - start_time
+    _display_total_stats(elapsed, len(args.FOLDERS), total_stats)
 
-    print('\nFinished.\n')
+    _print.totals('FINISHED.\n')
 
 if __name__ == '__main__':
     main()
