f123d905e70c0f47ce00ea1d87c429bf4808e235
nightjarrr <nightjarrr@dont.email.me.com>
2026-04-20 10:08:03 +0200

    Added support for --version flagin CLI.

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -1,5 +1,7 @@
 import pytest
 
+from importlib.metadata import PackageNotFoundError
+
 from molim import cli
 
 def test_cli_input_validation():
@@ -13,3 +15,24 @@ def test_cli_input_validation():
 def test_cli_dry_run():
     cli.run(["suffix", "--dry-run", "--verbose", ".anything", "."])
     assert True
+
+
+# Version support tests
+
+def test_version_when_package_installed(monkeypatch):
+    monkeypatch.setattr(cli, "version", lambda _: "1.2.3")
+    assert cli.__version() == "1.2.3"
+
+def test_version_when_package_not_installed(monkeypatch):
+    def raise_not_found(_):
+        raise PackageNotFoundError
+    monkeypatch.setattr(cli, "version", raise_not_found)
+    assert cli.__version() == cli.UNKNOWN_VERSION
+
+def test_cli_version_flag(monkeypatch, capsys):
+    monkeypatch.setattr(cli, "version", lambda _: "1.2.3")
+    with pytest.raises(SystemExit) as exc:
+        cli.run(["--version"])
+    assert exc.value.code == 0
+    captured = capsys.readouterr()
+    assert "molim 1.2.3" == captured.out.strip()
